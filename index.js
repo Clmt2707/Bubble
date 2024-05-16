17c83538301d503864d5aaf96b801f431e059202
Clmt2707 <clement.deboeuf@gmail.com>
2024-05-16 11:41:54 +0200

    fonction bubbleMaker

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-//Création d'une bulle de taille aléatoire
 
-const bubble = document.createElement('span');
-document.body.appendChild(bubble);
-bubble.classList.add('bubble');
 
-//Random sort un chiffre entre 0 et 1
-//Taille de bulles entre 100 et 300px
-const size = Math.random() * 200 + 100 + "px";
-bubble.style.height = size;
-bubble.style.width = size;
+const bubbleMaker = () => {
+    //Création d'une bulle de taille aléatoire 
+    const bubble = document.createElement('span');
+    document.body.appendChild(bubble);
+    bubble.classList.add('bubble');
 
-//Position aléatoire de la bulle/ apparition vers le bas de la page
-bubble.style.top = Math.random() * 100 + 50 + '%';
-bubble.style.left = Math.random() * 100 + '%';
+    //Random sort un chiffre entre 0 et 1
+    //Taille de bulles entre 100 et 300px
+    const size = Math.random() * 200 + 100 + "px";
+    bubble.style.height = size;
+    bubble.style.width = size;
 
-//Déplacement aléatoire
-bubble.style.setProperty('--left', Math.random() * 100 + '%');
+    //Position aléatoire de la bulle/ apparition vers le bas de la page
+    bubble.style.top = Math.random() * 100 + 50 + '%';
+    bubble.style.left = Math.random() * 100 + '%';
+
+    //Déplacement aléatoire
+    bubble.style.setProperty('--left', Math.random() * 100 + '%');
+};
+
+//création d'une bulle toutes les 2s
+/*setInterval(bubbleMaker, 300);*/
+
